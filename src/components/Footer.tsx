808fb30d38146680e775f86e73da0c96708fd1a9
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-03-11 13:49:17 +0100

    Rename stylesheets.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,12 @@
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import footerStyles from "../styles/Footer.module.css";
-import heartStyles from "../styles/Footer.module.css";
+import { FaHeart } from "react-icons/fa";
+import "../styles/Footer.css";
 
 function Footer() {
   return (
     <>
-      <footer className={footerStyles.footer}>
+      <footer className="footer">
         <p>
-          Made with{" "}
-          <FontAwesomeIcon className={heartStyles.heart} icon={faHeart} /> by
-          Jeffery
+          Made with <FaHeart className="heart" /> by Jeffery
         </p>
       </footer>
     </>
