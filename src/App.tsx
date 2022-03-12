b23db1cc0d9dd238f4c664e7fbe668b4bda4c0df
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-03-13 00:06:30 +0100

    Update Matches component style.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Matches from "./components/Matches/Matches";
